beea4cc47f0e8cbd88197128fd8ffb4839043cc7
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-04 16:23:37 +0100

    blocco 2 snack5

---
+++ js-snacks-blocco-2/snack5/javascript.js
@@ -0,0 +1,11 @@
+var arrUno = [1, 2, 3, 4, 5, 6, 7];
+var arrDue = [1, 2, 3, 4];
+
+while (arrUno.length > arrDue.length){
+
+    arrDue.push(Math.random());
+
+}
+
+console.log(arrUno);
+console.log(arrDue);
