922488040faa1e89d6e4cdb407a0baecf9803fb1
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-05 10:03:10 +0100

    migliorato snak5

--- js-snacks-blocco-2/snack5/javascript.js
+++ js-snacks-blocco-2/snack5/javascript.js
@@ -1,9 +1,22 @@
 var arrUno = [1, 2, 3, 4, 5, 6, 7];
 var arrDue = [1, 2, 3, 4];
 
-while (arrUno.length > arrDue.length){
+if (arrUno.length > arrDue.length){
 
-    arrDue.push(Math.random());
+  var major = arrUno;
+  var minor = arrDue;
+
+} else if (arrUno.length < arrDue.length){
+
+  var major = arrDue;
+  var minor = arrUno;
+
+}
+
+
+while (major.length > minor.length){
+
+  minor.push(Math.random());
 
 }
 
