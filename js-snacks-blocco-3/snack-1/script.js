10dbc5a7010e827d0c2049cc68edc8b3dcd80af6
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-19 10:16:30 +0100

    aggiustato

--- js-snacks-blocco-3/snack-1/script.js
+++ js-snacks-blocco-3/snack-1/script.js
@@ -1,33 +1,33 @@
 var bici = [
     {
         nome: "bici1",
-        peso: 3000,
+        peso: 3000
     },
     {
         nome: "bici2",
-        peso: 6000,
+        peso: 1000
     },
     {
         nome: "bici3",
-        peso: 1000,
+        peso: 9000
     },
     {
         nome: "bici4",
-        peso: 2000,
+        peso: 2000
     },
     {
         nome: "bici5",
-        peso: 3500,
+        peso: 500
     }
 ]
 
 
-var biciPeso= bici[0]["peso"];
+var biciPeso= bici[0];
 for (var i = 0; i < bici.length; i++) {
      
-    if (bici[i]["peso"] < biciPeso ) {
+    if (bici[i]["peso"] < biciPeso["peso"] ) {
 
-        biciPeso = bici[i]["nome"];
+        biciPeso = bici[i];
 
     }
     
